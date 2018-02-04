024da7894b26eb9d433f3873c57a4600456f8c74
AlekseyLeshko <alekseyleshko@gmail.com>
2018-02-04 17:50:32 +0400

    Install YCM plugin

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
+basePath=`pwd`
 
 clear
 
@@ -6,5 +7,11 @@ echo "Start install"
 
 echo "Install brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "Install YCM"
+cd ~/.vim/bundle/YouCompleteMe
+./install.py --clang-completer --js-completer
+
+cd $basePath
 
 echo "Finish install"
