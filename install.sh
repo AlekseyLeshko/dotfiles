ddd4e807cdfbf2044dfc06374d6d60e693fef173
AlekseyLeshko <alekseyleshko@gmail.com>
2018-02-06 23:54:54 +0400

    Install global npm packages

--- install.sh
+++ install.sh
@@ -14,4 +14,7 @@ cd ~/.vim/bundle/YouCompleteMe
 
 cd $basePath
 
+echo "Install global npm packages"
+yarn global add eslint eslint_d pm2 instant-markdown-d react-native-cli lock-cli alfred-lock
+
 echo "Finish install"
