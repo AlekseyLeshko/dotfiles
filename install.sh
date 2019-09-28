e2d9a8363d9e3607661886904524316b6865fd44
AlekseyLeshko <alekseyleshko@gmail.com>
2019-09-28 15:37:29 +0300

    Add step for install nodejs

--- install.sh
+++ install.sh
@@ -39,6 +39,9 @@ brew install yarn
 echo "Install nvm"
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 
+echo "Install nodejs"
+nvm install v10.10.0
+
 echo "Install YCM"
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer --js-completer
