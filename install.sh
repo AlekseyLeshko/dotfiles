96a380320f7c2b509d48cf25f0c729317efbff53
AlekseyLeshko <alekseyleshko@gmail.com>
2019-09-28 13:58:20 +0300

    Add debug info

--- install.sh
+++ install.sh
@@ -27,7 +27,10 @@ echo "Start install"
 echo "Install brew"
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Install silver searcher"
 brew install the_silver_searcher
+
+echo "Install fuzzy finder"
 brew install fzf
 
 echo "Install nvm"
